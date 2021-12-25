eda23b101d0813195ab2b827b0307d8d1f415397
Catalin Micu <catalinmicu98@gmail.com>
2021-12-25 14:28:35 +0200

    initial commit

---
+++ flask/__init__.py
@@ -0,0 +1,1 @@
+# todo: figure this shit out
---
+++ initialize_flask_server.py
@@ -0,0 +1,8 @@
+
+
+def print_hi(name):
+    print(f'Hi, {name}\nTime to run this flask API bitch')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('bitches')
