3eb88c5a2b0664d355f3ee332c919035c5377d3a
Catalin Micu <catalinmicu98@gmail.com>
2022-05-23 01:14:16 +0300

    stuff

--- initialize_flask_server.py
+++ initialize_flask_server.py
@@ -4,16 +4,6 @@ from flask_server.model.users import Users
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mbdedproject'
-# app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-#
-# # Set the cookie paths, so that you are only sending your access token
-# # cookie to the access endpoints, and only sending your refresh token
-# # to the refresh endpoint. Technically this is optional, but it is in
-# # your best interest to not send additional cookies in the request if
-# # they aren't needed.
-# app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
-# app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
 
 # Disable CSRF protection for this example. In almost every case,
 # this is a bad idea. See examples/csrf_protection_with_cookies.py
